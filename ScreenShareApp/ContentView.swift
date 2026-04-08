1459a90477b19a2d6cd8e59167f43275483a8a3a
kyuchanOh <107012197+kyuchanOh@users.noreply.github.com>
2026-04-08 16:44:34 +0900

    Initial Commit

---
+++ ScreenShareApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScreenShareApp
+//
+//  Created by supercoder on 4/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScreenShareAppTests/ScreenShareAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenShareAppTests.swift
+//  ScreenShareAppTests
+//
+//  Created by supercoder on 4/8/26.
+//
+
+import Testing
+@testable import ScreenShareApp
+
+struct ScreenShareAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
