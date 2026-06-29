c93d5649909332003685e003cf397a556751e8de
willjun0115 <willjun0115@gmail.com>
2026-06-29 14:19:25 +0900

    MODIFY: renew remoteVideoView

--- ScreenShareApp/RemoteVideoView.swift
+++ ScreenShareApp/RemoteVideoView.swift
@@ -13,6 +13,7 @@ struct RemoteVideoView: UIViewRepresentable {
         let view = RTCMTLVideoView(frame: .zero)
         view.videoContentMode = .scaleAspectFit // 비율 유지하며 꽉 차게 표시
         view.backgroundColor = .black
+        view.delegate = context.coordinator
         return view
     }
 
@@ -20,6 +21,22 @@ struct RemoteVideoView: UIViewRepresentable {
         // 기존 트랙 제거 후 새 트랙 연결
         if let track = videoTrack {
             track.add(uiView)
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, RTCVideoViewDelegate {
+        var parent: RemoteVideoView
+
+        init(_ parent: RemoteVideoView) {
+            self.parent = parent
+        }
+
+        func videoView(_ videoView: RTCVideoRenderer, didChangeVideoSize size: CGSize) {
+            NSLog("✅ [RemoteVideoView] 첫 프레임 도달 또는 해상도 변경됨: \(size.width) x \(size.height)")
         }
     }
 }
